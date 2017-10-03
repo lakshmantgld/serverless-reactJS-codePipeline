e92f6f512a416178e55d34d51821e9873ba230a8
Lakshman <lakshmanTgld>
2017-10-03 23:55:58 +0900

    failure slack notifier added

--- codeBuild/handler.js
+++ codeBuild/handler.js
@@ -25,3 +25,26 @@ module.exports.successNotifier = (event, context, callback) => {
   });
 
 };
+
+module.exports.failureNotifier = (event, context, callback) => {
+
+  console.log("Inside failureNotifier");
+  console.log("Printing SNS message: ", event["Records"][0]["Sns"]["Message"]);
+  console.log("Printing the whole event");
+  console.log(event);
+
+  const options = {
+    text: event["Records"][0]["Sns"]["Message"].replace(/^"(.*)"$/, '$1'),
+  };
+
+  axios.post(process.env.FAILURE_SLACK_CHANNEL, JSON.stringify(options))
+  .then((response) => {
+    console.log('\nSUCCEEDED: Slack Message to Failure Channel: \n', response.data);
+    callback(null, 'SUCCEEDED: Slack Message to Failure Channel');
+  })
+  .catch((error) => {
+    console.log('\nFAILED: Slack Message to Failure Channel', error);
+    callback(error);
+  });
+
+};
